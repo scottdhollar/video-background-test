b3eddb535aec330838e4dc116c5fd98ca7943067
tertiux44 <secondsonofadebayo@gmail.com>
2022-10-15 10:34:10 +0100

    added cool shit

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 // adding backdrop to every other element
-var mybackdrop = document.querySelector(".backdrop")
+// var mybackdrop = document.querySelector(".backdrop")
 
 // function kaisen() {
 //     mybackdrop.classList.toggle("show-backdrop")
 // }
+
+var unavailablefeature = "This feature is not yet available"
+
+function dosomething() {
+    alert(unavailablefeature)
+}
+
+var contact= "https://wa.me/message/SQWCD3JZAO36B1"
+
+function contactus() {
+    window.open (contact)
+}
