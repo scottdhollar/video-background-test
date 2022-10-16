51191ed4a47cba674d8e165a5f5c66ee3468e6c8
tertiux44 <secondsonofadebayo@gmail.com>
2022-10-16 04:21:49 +0100

    mobile view

--- main.js
+++ main.js
@@ -5,14 +5,27 @@
 //     mybackdrop.classList.toggle("show-backdrop")
 // }
 
-var unavailablefeature = "This feature is not yet available"
+// var contact= "https://wa.me/message/SQWCD3JZAO36B1"
 
+// function contactus() {
+//     window.open (contact)
+// }
+var unsupportedscreen = "Webpage is not optimized for your device"
+var unavailablefeature = "This feature is not yet available"
+var hamburgerdiv = document.querySelector(".hamburger")
+var mobileLinks = document.querySelector(".mobile-link-holder")
+var mybackdrop = document.querySelector(".backdrop")
+
+
+function kaisen() {
+    hamburgerdiv.classList.toggle("showburger")
+    mobileLinks.classList.toggle("show-mobile-link-holder")
+    mybackdrop.classList.toggle("show-backdrop")
+}
 function dosomething() {
     alert(unavailablefeature)
 }
-
-var contact= "https://wa.me/message/SQWCD3JZAO36B1"
-
-function contactus() {
-    window.open (contact)
+function invalidscreen() {
+    alert(unsupportedscreen)
 }
+
